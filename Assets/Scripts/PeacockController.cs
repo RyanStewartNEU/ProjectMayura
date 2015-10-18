8af08a44c741ccf132caad838b3c170a98ac07cc
RyanStewartNEU <stewart.rya@husky.neu.edu>
2015-10-18 16:53:48 -0400

    Input Setup and basic movement script

---
+++ Assets/Scripts/PeacockController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PeacockController : MonoBehaviour {
+    public Camera camera;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 moveDirection = Vector3.zero;
+        
+        //Grounded Movement
+        if(GetComponent<CharacterController>().isGrounded) {
+            //GetComponent<CharacterController>().Move(new Vector3(0, 0, Input.GetAxis("Vertical Movement") * Time.deltaTime));
+            moveDirection = new Vector3(0, 0, Input.GetAxis("Vertical Movement")*6f);
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= 8f;
+            //Jump
+            if (Input.GetButton("Jump")) {
+                moveDirection.y = 8f;
+            }
+        }
+        moveDirection.y -= 20f * Time.deltaTime;
+        GetComponent<CharacterController>().Move(moveDirection * Time.deltaTime);
+
+    }
+}
