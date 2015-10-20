186d17a7f0af8f3b21e4ee79cad021cc2c9c9f61
dean thurston <deanthurston2791@gmail.com>
2015-10-20 00:15:36 -0400

    Fixed up controls and added camera movement
    Can now move while jumping and can hold jump to jump longer

---
+++ Assets/MouseAimCamera.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseAimCamera : MonoBehaviour {
+    public GameObject target;
+    public float rotateSpeed = 5;
+    float vertical;
+    Vector3 offset;
+     
+    void Start() 
+    {
+        offset = target.transform.position - transform.position;
+    	vertical = transform.eulerAngles.x;
+    }
+    
+    void CheckCameraCollision()
+    {
+	    Ray ray=new Ray(target.position, transform.position - target.position);
+	    RaycastHit hit = new RaycastHit();
+	    if(Physics.Raycast(ray, out hit, (transform.position - target.position).magnitude)){
+        
+        if(hit.distance > offset)
+            transform.position=ray.GetPoint(hit.distance - offset);
+        else
+            transform.position=target.position;
+    }
+
+    void LateUpdate() 
+    {
+        float horizontal = Input.GetAxis("Horizontal Camera") * rotateSpeed;
+        vertical += Input.GetAxis("Vertical Camera") * rotateSpeed;
+        target.transform.Rotate(0, horizontal, 0);
+       
+        Quaternion rotation = Quaternion.Euler(vertical, target.transform.eulerAngles.y, 0);
+        transform.position = target.transform.position - (rotation * offset);
+         
+        transform.LookAt(target.transform);
+    }
+}
--- Assets/Scripts/PeacockController.cs
+++ Assets/Scripts/PeacockController.cs
@@ -5,6 +5,7 @@ public class PeacockController : MonoBehaviour {
     public Camera camera;
     public float acceleration, jumpSpeed;
     public float maxSpeed;
+    public float holdSpeed;
     CharacterController controller;
 	Vector3 moveDirection;
     // Use this for initialization
@@ -15,36 +16,47 @@ public class PeacockController : MonoBehaviour {
     }
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+    {
                 
+        // get current XZ movement
+        Vector3 addedVel = (Input.GetAxisRaw("Vertical Movement")  * transform.forward) + (Input.GetAxisRaw("Horizontal Movement") * transform.right);
+        addedVel *= acceleration; // apply accelration rate
+        moveDirection += addedVel; // add to move
+        
+
+        Vector2 XZ  = new Vector2(moveDirection.x,moveDirection.z);
+        if(XZ.magnitude > maxSpeed) //if we are moving faster than the top speed
+        {
+            XZ = XZ.normalized * maxSpeed;
+            moveDirection = new Vector3(XZ.x,moveDirection.y,XZ.y); // scale to move at top speed
+        }
         //Grounded Movement
         if(controller.isGrounded) 
         {   
-            Vector3 addedVel = (Input.GetAxisRaw("Vertical Movement")  * transform.forward) + (Input.GetAxisRaw("Horizontal Movement") * transform.right);
-            addedVel *= acceleration;
-            moveDirection += addedVel;
-
-            if(addedVel.magnitude == 0)
+            if(addedVel.magnitude == 0) // if we arent moving
             {
-                if(moveDirection.magnitude < 1)
-                moveDirection = Vector3.zero;
-                else
-                moveDirection*=.9f;
+                if(moveDirection.magnitude < 1) // if we are close to stopped
+                moveDirection = Vector3.zero; // stop
+                else // apply friction
+                moveDirection*=.8f;
             }
 
-            if(moveDirection.magnitude > maxSpeed)
-                moveDirection = moveDirection.normalized * maxSpeed;
-            
-            
-            if(Input.GetAxisRaw("Jump") > 0)
+                  
+            if(Input.GetAxisRaw("Jump") > 0) // if the jump button is pressed
             {
-                moveDirection.y = jumpSpeed;
+                moveDirection.y = jumpSpeed; // apply jump to move
             }             
+        }
+        else
+        {
+            if(Input.GetAxisRaw("Jump") > 0 && moveDirection.y > 0) // if the jump button is being held
+            {
+                moveDirection.y += holdSpeed; // move upwards slightly, for hold jumps
+            }   
         }
 
         moveDirection.y += Physics.gravity.y * Time.deltaTime;
         controller.Move(moveDirection * Time.deltaTime);
-        //if(controller.velocity.magnitude )
-        
     }
 }
