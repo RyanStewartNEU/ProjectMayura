ae871e8235db039079c652ba00b0383096386751
Zack Hickman <zdhickman@gmail.com>
2015-10-26 13:12:07 -0400

    refactor and clean up PeacockController

--- Assets/Scripts/PeacockController.cs
+++ Assets/Scripts/PeacockController.cs
@@ -2,6 +2,13 @@
 using System.Collections;
 
 public class PeacockController : MonoBehaviour {
+
+	public static float AXES_THRESHOLD = 0.3F;
+	public static string JUMP_BUTTON_STRING = "Jump";
+	public static string VERTICAL_MOVEMENT_STRING = "Vertical Movement";
+	public static string HORIZONTAL_MOVEMENT_STRING = "Horizontal Movement";
+	public static int MOVEMENT_COEFFICIENT = 4;
+
     public Camera camera;
     public Transform model;
     public Transform lookTransform;
@@ -19,9 +26,9 @@ public class PeacockController : MonoBehaviour {
     Vector2 lastPosition;
     float jumpTime;
     bool holdingJump;
+
     // Use this for initialization    
-    void Start () 
-    {
+    void Start () {
        controller = transform.GetComponent<CharacterController>();
        moveDirection = Vector3.zero;
        lastPosition = transform.position;
@@ -30,110 +37,56 @@ public class PeacockController : MonoBehaviour {
 
 
     // Update is called once per frame
-    void Update () 
-    {
-                
-        float xAxis = 0;
-        float yAxis = 0;
-        if(Mathf.Abs(Input.GetAxis("Vertical Movement")) > 0.3f)
-        {
-            xAxis = Input.GetAxis("Vertical Movement");
-        }
+    void Update () {   
+		float xAxis = getVerticalInput();
+		float yAxis = getHorizontalInput();
+		Vector3 addedVel = getXZMovement();
+		Vector2 axisVector = new Vector2(xAxis, yAxis);
+		float axisVectorMagnitude = axisVector.magnitude;
+        float speed = axisVectorMagnitude * rate;
 
-        if(Mathf.Abs(Input.GetAxis("Horizontal Movement")) > 0.3f)
-        {
-            yAxis = Input.GetAxis("Horizontal Movement");
-        }
-        // get current XZ movement
-        Vector3 yAx = (yAxis * camera.transform.right);
-        yAx.y = 0;
-        Vector3 addedVel = (xAxis  * camera.transform.forward) + yAx;  
-        addedVel.y = 0;
+        if (controller.isGrounded) {
+            Vector2 axisChange = axisVector - prevAxis;
 
-        float speed = new Vector2(xAxis,yAxis).magnitude * rate;
-        
-
-        if(controller.isGrounded)
-        {
-            Vector2 axisChange = new Vector2(xAxis,yAxis) - prevAxis;
-            if(Mathf.Abs(axisChange.x) > 1  || Mathf.Abs(axisChange.y) > 1 && prevAxis.magnitude > 0.25)
-            {
+            if (went(axisChange)) {
                 Debug.Log("went");
-                moveDirection += addedVel * speed * 4;     
-            }
-            else
-            moveDirection += addedVel * speed;
-        }
-        else
-        {
-            addedVel *= acceleration; // apply accelration rate
+                moveDirection += MOVEMENT_COEFFICIENT * addedVel * speed;     
+            } else {
+            	moveDirection += addedVel * speed;
+			}
+        } else {
+            addedVel *= acceleration; // apply acceleration rate
             moveDirection += addedVel; // add to move
         } 
-
         
-        Vector2 XZ  = new Vector2(moveDirection.x,moveDirection.z);
-        float scale = new Vector2(xAxis,yAxis).magnitude / Mathf.Sqrt(2);
-        scale *= scale;
-        if(scale < 0.3f) scale = 0.3f;
-        if(XZ.magnitude > maxSpeed * scale) //if we are moving faster than the top speed
-        {
+        Vector2 XZ  = new Vector2(moveDirection.x, moveDirection.z);
+		float scale = Mathf.Max(Mathf.Pow(axisVectorMagnitude / Mathf.Sqrt(2), 2), AXES_THRESHOLD);
+        
+		// if we are moving faster than the top speed
+		if (XZ.magnitude > maxSpeed * scale) {
             XZ = XZ.normalized * maxSpeed * scale;
-            Vector3 wantedMovement = new Vector3(XZ.x,moveDirection.y,XZ.y);
-            moveDirection =  wantedMovement;//Vector3.Slerp(moveDirection,wantedMovement,0.5f);// scale to move at top speed
+            Vector3 wantedMovement = new Vector3(XZ.x, moveDirection.y, XZ.y);
+            moveDirection = wantedMovement; // scale to move at top speed
         }
 
-        //Grounded Movement
-        if(controller.isGrounded) 
-        {   
-            if(addedVel.magnitude == 0) // if we arent moving
-            {
-                if(moveDirection.magnitude < 1) // if we are close to stopped
-                    moveDirection = Vector3.zero; // stop
-                else // apply friction
-                    moveDirection*= friction;
-            }
-           
-            if(Input.GetButtonDown("Jump")) // if the jump button is pressed
-            {
-                moveDirection.y = jumpSpeed; // apply jump to move
-                jumpTime = Time.time;
-                holdingJump = true;
-            }             
+        // Grounded Movement
+        if (controller.isGrounded) {   
+			doGroundedMovement(addedVel);            
+        } else {
+			doAirMovement(); 
         }
-        else
-        {
-            if(!Input.GetButton("Jump") ||  jumpTime < Time.time - holdTime) // if the jump button is being held
-            {
-                holdingJump = false;
-            }
 
-            if(holdingJump)
-            {
-                moveDirection.y = jumpSpeed; // move upwards slightly, for hold jumps 
-            }
-             moveDirection.y -= gravity;  
-        }
-        
-       
+		Vector2 moveDirectionVector = new Vector2 (moveDirection.x, moveDirection.z);
       
-        if(new Vector2(moveDirection.x,moveDirection.z).magnitude != 0 && controller.isGrounded)
-        {
-            
-            
-            Vector2 dir = new Vector2(moveDirection.x,moveDirection.z).normalized;
-            lookDir = Vector3.RotateTowards(model.forward, new Vector3(dir.x,0,dir.y), deltaRotation * Time.deltaTime, 0.0F);
+        if (moveDirectionVector.magnitude > 0 && controller.isGrounded) {
+            Vector2 dir = moveDirectionVector.normalized;
+            lookDir = Vector3.RotateTowards(model.forward, new Vector3(dir.x, 0, dir.y), deltaRotation * Time.deltaTime, 0F);
             lookTransform.position = model.position + lookDir;
-            //float ang = Mathf.Atan2(dir.y, dir.x);
-          // Debug.Log(dir + " " +  new Vector2(newDir.x, newDir.z));
-         // Debug.Log(lookDir + " " + dir);
-           model.LookAt(lookTransform);
+           	model.LookAt(lookTransform);
         }
         
-        prevAxis  = new Vector2(xAxis,yAxis);      
+        prevAxis = axisVector;      
         controller.Move(moveDirection * Time.deltaTime);
-
-        
-
     }
 
     public void LateUpdate()
@@ -141,4 +94,77 @@ public class PeacockController : MonoBehaviour {
         lastPosition = new Vector2(transform.position.x,transform.position.z);
   
     }
+
+	private float getVerticalInput() {
+		float xInput = Input.GetAxis(VERTICAL_MOVEMENT_STRING);
+		
+		if (Mathf.Abs(xInput) > AXES_THRESHOLD) {
+			return xInput;
+		}
+
+		return 0;
+	}
+
+	private float getHorizontalInput() {
+		float yInput = Input.GetAxis(HORIZONTAL_MOVEMENT_STRING);
+		
+		if (Mathf.Abs(yInput) > AXES_THRESHOLD) {
+			return yInput;
+		}
+		
+		return 0;
+	}
+
+	private Vector3 getXZMovement(float xAxis, float yAxis) {
+		Vector3 yAx = (yAxis * camera.transform.right);
+		yAx.y = 0;
+		Vector3 addedVel = (xAxis * camera.transform.forward) + yAx;  
+		addedVel.y = 0;
+
+		return addedVel;
+	}
+
+	// TODO: rename this plz
+	private void went(Vector2 axisChange) {
+		return Mathf.Abs(axisChange.x) > 1  
+			|| Mathf.Abs(axisChange.y) > 1 
+				&& prevAxis.magnitude > 0.25;
+	}
+
+	private float getMagnitude(float x, float y) {
+		return new Vector2(x, y).magnitude;
+	}
+
+	private void doGroundedMovement(Vector3 addedVel) {
+		// if we arent moving
+		if (addedVel.magnitude == 0) {
+			
+			// if we are close to stopped
+			if (moveDirection.magnitude < 1) {
+				moveDirection = Vector3.zero; // stop
+			} else {
+				moveDirection *= friction; // apply friction
+			}
+		}
+		
+		// if the jump button is pressed
+		if (Input.GetButtonDown (JUMP_BUTTON_STRING)) {
+			moveDirection.y = jumpSpeed; // apply jump to move
+			jumpTime = Time.time;
+			holdingJump = true;
+		} 
+	}
+
+	private void doAirMovement() {
+		// if the jump button is being held
+		if (!Input.GetButton(JUMP_BUTTON_STRING) ||  jumpTime < Time.time - holdTime) {
+			holdingJump = false;
+		}
+		
+		if (holdingJump) {
+			moveDirection.y = jumpSpeed; // move upwards slightly, for hold jumps 
+		}
+		
+		moveDirection.y -= gravity; 
+	}
 }
