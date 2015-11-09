02e58d1559dc4aefdfd49a5f9ae5f58a4d3c216c
RyanStewartNEU <stewart.rya@husky.neu.edu>
2015-11-09 13:25:39 -0500

    Full Update from my branch

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingPlatform : MonoBehaviour {
+
+    public Transform moveTo;
+    public float moveSpeed = .1f;
+    private Vector3 startFrom;
+    public bool movingAway = true;
+	// Use this for initialization
+	void Start () {
+        startFrom = this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (movingAway)
+        {
+            if (Vector3.Distance(this.transform.position, moveTo.position) < 0.1f)
+            {
+                movingAway = false;
+            }
+            else
+            {
+                this.transform.position = Vector3.Lerp(this.transform.position, moveTo.position, moveSpeed);
+            }
+        }
+        else
+        {
+            if (Vector3.Distance(this.transform.position, startFrom) < 0.1f)
+            {
+                movingAway = true;
+            }
+            else
+            {
+                this.transform.position = Vector3.Lerp(this.transform.position, startFrom, moveSpeed);
+            }
+        }
+	}
+}
